d61c955ed1563f4fd111339d2225570aa32567dc
candipy <88384618+candipy@users.noreply.github.com>
2022-06-09 16:50:57 +0200

    Signup without login (!==errors)

--- front/src/views/SignupView.vue
+++ front/src/views/SignupView.vue
@@ -2,63 +2,79 @@
   <Header />
   <nav>
     <router-link to="/" class="btn-grad">Home</router-link>
-
-    <router-link to="/login" class="btn-grad">Login</router-link>
   </nav>
 
-  <div class="container_principal">
+  <div class="card">
+    <!-- Titre -->
+
     <h1>Création de votre compte</h1>
 
-    <div class="form">
-      <form @submit.prevent="login" method="post">
+    <!--  Lien vers Login -->
+    <div class="card_subtitle">
+      <p>Vous avez déjà un compte ?</p>
+      <router-link to="/login">Se connecter</router-link>
+    </div>
+
+    <!-- Formulaire de connexion / création de compte -->
+    <div class="card">
+      <form @submit.prevent method="post" class="form">
         <div class="form__input">
-          <label for="lastName">Votre nom</label>
+          <label for="lastName">Nom</label>
           <input
-            @change="checkLastName"
             type="text"
             v-model="lastName"
+            @change="checkLastName"
             name="lastName"
             id="lastName"
             aria-label="last_name"
             placeholder="Indiquez votre nom"
             required
           />
-          <p v-show="errorLastName" id="form__error">
+          <p v-if="errorLastName" class="form__error">
             Merci d'entrer un nom conforme. Ex : Dupond, Du-Pond, Du Pond
           </p>
         </div>
+
         <div class="form__input">
-          <label for="firstName">Votre prénom</label>
+          <label for="firstName">Prénom</label>
           <input
-            @change="checkfirstName"
             type="text"
             v-model="firstName"
+            @change="checkFirstName"
             name="firstName"
             id="fistName"
             aria-label="first_name"
             placeholder="Indiquez votre prénom"
             required
           />
-          <p v-show="errorFirstName" class="form__error">
+          <p v-if="errorFirstName" class="form__error">
             Merci d'entrer un prénom conforme. Ex : Jean, Jean-François, Jean
             François
           </p>
         </div>
         <div class="form__input">
-          <label for="email">Votre email</label>
+          <label for="email">Email</label>
           <input
-            @change="checkEmail"
             type="email"
             v-model="email"
+            @change="checkEmail"
             name="email"
             id="email"
             aria-label="email_signup"
             placeholder="Sera votre identifiant"
             required
           />
-          <p v-show="errorEmail" class="form__error">
+          <p v-if="errorEmail" class="form__error">
             Merci d'entrer un courriel conforme. Ex : contact@groupomania.com
           </p>
+
+          <!-- <div v-if="status == 'error_email'" class="form__error">
+            Merci d'entrer un courriel conforme. Ex : contact@groupomania.com
+          </div> -->
+          <div v-if="status == 'error_unique'" class="form__error">
+            Vous avez déjà un compte avec cette adresse mail, retournez à la
+            page de connexion
+          </div>
         </div>
         <div class="form__input">
           <label for="passwordSignup">Mot de passe</label>
@@ -68,95 +84,153 @@
             name="password"
             id="passwordSignup"
             aria-label="password_signup"
-            placeholder="Choisissez votre mot de passe"
+            placeholder="Indiquez un mot de passe"
             required
           />
-          <p class="form__error"><!-- validation --></p>
+
+          <div v-if="status == 'error_password'" class="form__input">
+            Votre mot de passe doit comprendre entre 8 et 200 caractères, une
+            majuscule, une minuscule, et aucun espace
+          </div>
+        </div>
+        <div v-if="status == 'error_serveur'" class="form__input">
+          Une erreur inconnue s'est produite, veuillez reessayer plus tard ou
+          contactez votre administrateur
         </div>
 
-        <input type="submit" class="btn-grad" value="Valider" />
+        <button
+          :class="classIs"
+          :disabled="isDisabledAttribute"
+          @click="createAccount()"
+        >
+          <span v-if="status == 'loading'"> Création en cours</span>
+          <span v-else>Valider</span>
+        </button>
       </form>
     </div>
   </div>
 </template>
 
 <script>
-import Header from "../components/Header.vue";
+import Header from "@/components/Header.vue";
+
+import { mapState } from "vuex";
 
 export default {
-  name: "Signup",
+  name: "Login",
   components: {
     Header,
   },
 
-  data() {
+  data: () => {
     return {
+      // Saisie Utilisateur
       lastName: "",
-      errorLastName: false,
       firstName: "",
-      errorFirstName: false,
       email: "",
-      errorEmail: false,
       password: "",
-      regexAlpha: new RegExp(/^[A-Za-zÀ-ž-'\s]+$/), // Est ce une bonne pratique ?
-      regexEmail: new RegExp(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+.[a-zA-Z]{2,4}$/),
+      // Erreurs
+      errorLastName: false,
+      errorFirstName: false,
+      errorEmail: false,
+      // Bouton Connexion => Changement de class
+      btnIsActive: false,
     };
   },
 
-  methods: {
-    checkLastName() {
-      if (this.regexAlpha.test(this.lastName)) {
-        this.errorLastName = false;
-        return false;
+  computed: {
+    // Activation du bouton CONNEXION
+    classIs() {
+      if (
+        this.errorLastName ||
+        this.errorFirstName ||
+        this.errorEmail ||
+        this.password == ""
+      ) {
+        return "btn-grad--disabled";
       } else {
-        this.errorLastName = true;
-        return true;
+        return "btn-grad";
       }
     },
 
-    checkfirstName() {
-      if (this.regexAlpha.test(this.firstName)) {
-        this.errorFirstName = false;
+    isDisabledAttribute() {
+      if (
+        this.errorLastName ||
+        this.errorFirstName ||
+        this.errorEmail ||
+        this.password == ""
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+
+    ...mapState(["status"]),
+  },
+
+  methods: {
+    // Vérification de la conformité des champs
+    regexAlpha(value) {
+      let regexAlpha = /^[A-Za-zÀ-ž-'\s]+$/;
+      return regexAlpha.test(value);
+    },
+
+    regexEmail(value) {
+      return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9-]+.[a-zA-Z]{2,4}$/.test(value);
+    },
+
+    checkLastName() {
+      // Si après le changement il y a un regex non conforme ou un champ
+      if (!this.regexAlpha(this.lastName) || this.lastName == "") {
+        this.errorLastName = true; // Il y a une erreur et le v-if de l'erreur s'affiche
+        return false; // La vérification à échouée, il y a une erreur
+      } else {
+        this.errorLastName = false; // Mis ca car si je met thsi.errorLastName sans = false, si le user à en premier lieu fait une erreur errorLastName= true donc le v-if continuerait de s'afficher
+        return true; //La vérification à réussi, il n'y a pas d'erreur
+      }
+    },
+    checkFirstName() {
+      if (!this.regexAlpha(this.firstName) || this.firstName == "") {
+        this.errorFirstName = true;
         return false;
       } else {
-        this.errorFirstName = true;
+        this.errorFirstName = false;
         return true;
       }
     },
 
     checkEmail() {
-      if (this.regexEmail.test(this.email)) {
-        this.errorEmail = false;
+      if (!this.regexEmail(this.email) || this.email == "") {
+        this.errorEmail = true;
         return false;
       } else {
-        this.errorEmail = true;
+        this.errorEmail = false;
         return true;
       }
     },
 
-    async login() {
-      const { password, firstName, lastName, email } = this;
-      const res = await fetch("http://localhost:3000/api/auth/signup/", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          firstName,
-          lastName,
-          email,
-          password,
-        }),
-      });
-      const data = await res.json();
-      console.log(data);
+    // Création de compte
+    createAccount() {
+      this.$store
+        .dispatch("createAccount", {
+          firstName: this.firstName,
+          lastName: this.lastName,
+          email: this.email,
+          password: this.password,
+        })
+        .then(
+          (userCreate) => {
+            console.log("UserCreate :>> ", userCreate);
+          },
+          (error) => {
+            console.log("UserCreate-Error :>> ", error);
+          }
+        );
     },
   },
 };
 </script>
 
 <style lang="scss">
-// @import "@/css/mains.scss";
-
-
 </style>
